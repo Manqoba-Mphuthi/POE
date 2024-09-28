39dee93cee61d80fd918b01681b639496be251b3
RC_Student_lab <RC_Student_lab@10.0.0.36>
2024-09-28 19:29:05 +0000

    Method that registers a user

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -22,11 +22,29 @@ public class Login {
     //Method that checks if password meets complexity requirements ( >=8 characters long, contain capital letter, a number, and a special character)
     
     public boolean checkPasswordComplexity(String password) {
-        //check if password meets complexity requirements
-        return password.length() >= 8
+               return password.length() >= 8
                   && password.matches(".[A-Z].")
                   && password.matches(".[0-9].")
                   && password.matches(".[^a-zA-Z0-9].");
+    }   
+    //Method that registers a user
+    
+    public String registerUser(String username, String password, String firstName, String lastName) {
+    
+    //If statements that check username and password formats
+    
+    if (!checkUserName(username)) {
+        return "Username is not correctly formatted.";
+    }
+    if (!checkPasswordComplexity(password)) {
+        return "Password is not correctly formatted.";
+    }
+    
+    //Create a new user and store it
+    
+    registeredUser = new User(username, password, firstName, lastName);
+        return "User registered successfully.";
+    
     }
 }
 
