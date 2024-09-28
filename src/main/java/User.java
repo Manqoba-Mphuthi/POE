bbbb5eb829015bae664788dd857186e955e0f5ee
RC_Student_lab <RC_Student_lab@10.0.0.36>
2024-09-28 16:37:57 +0000

    Declaration of variables and constructor.

---
+++ src/main/java/User.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class User {
+    
+    //Declaration of varaiables
+    
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+    //Constructor
+    
+    public User(String username, String password, String firstName, String lastName ) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
