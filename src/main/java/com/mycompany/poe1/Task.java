374e69979674ce3e619f3736d1e225302733b437
RC_Student_lab <RC_Student_lab@10.0.0.36>
2024-10-20 20:00:42 +0000

    Constructor for task variable

--- src/main/java/com/mycompany/poe1/Task.java
+++ src/main/java/com/mycompany/poe1/Task.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
  * @author RC_Student_lab
  */
 public class Task {
+
 //Declaration of task-related variables
 private String taskName;
 private int taskNumber;
@@ -18,4 +19,16 @@ private String developerLastName;
 private int taskDuration;
 private String taskID;
 private String taskStatus;
+
+//Constructor for task variables
+public Task(String taskName, int taskNumber, String taskDescription, String developerFirstName, String developerLastName, int taskDuration, String taskID, String taskStatus) {
+    this.taskName = taskName;
+    this.taskNumber = taskNumber;
+    this.taskDescription = taskDescription;
+    this.developerFirstName = developerFirstName;
+    this.developerLastName = developerLastName;
+    this.taskDuration = taskDuration;
+    this.taskID = taskID;
+    this.taskStatus = taskStatus;
+}
 }
