6884faa8c6bd33bda2ab7a04c2360e25054879c9
RC_Student_lab <RC_Student_lab@10.0.0.36>
2024-10-20 19:37:59 +0000

    Declaration of task-related variables

---
+++ src/main/java/com/mycompany/poe1/Task.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poe1;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Task {
+//Declaration of task-related variables
+private String taskName;
+private int taskNumber;
+private String taskDescription;
+private String developerFirstName;
+private String developerLastName;
+private int taskDuration;
+private String taskID;
+private String taskStatus;
+}
